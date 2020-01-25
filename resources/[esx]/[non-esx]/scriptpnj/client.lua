1ab98bcc11adf275a63a3426c513f151023d3307
JericoFX <56100605+JericoFX@users.noreply.github.com>
2020-01-25 20:03:25 -0300

    Actualizado la densidad de los pj

--- resources/[esx]/[non-esx]/scriptpnj/client.lua
+++ resources/[esx]/[non-esx]/scriptpnj/client.lua
@@ -3,13 +3,13 @@ Citizen.CreateThread(function()
 		Citizen.Wait(0) -- prevent crashing
 
 		-- These natives have to be called every frame.
-		SetPedDensityMultiplierThisFrame(0.1) -- set npc/ai peds density to 0
-		SetRandomVehicleDensityMultiplierThisFrame(0.1) -- set random vehicles (car scenarios / cars driving off from a parking spot etc.) to 0
-		SetScenarioPedDensityMultiplierThisFrame(0.0, 0.0) -- set random npc/ai peds or scenario peds to 0
+		SetPedDensityMultiplierThisFrame(0.9) -- set npc/ai peds density to 0
+		SetRandomVehicleDensityMultiplierThisFrame(0.5) -- set random vehicles (car scenarios / cars driving off from a parking spot etc.) to 0
+		SetScenarioPedDensityMultiplierThisFrame(0.8, 0.8) -- set random npc/ai peds or scenario peds to 0
 		SetVehicleModelIsSuppressed(GetHashKey("rubble"), true)
         SetVehicleModelIsSuppressed(GetHashKey("taco"), true)
         SetVehicleModelIsSuppressed(GetHashKey("biff"), true)
-		SetGarbageTrucks(false) -- Stop garbage trucks from randomly spawning
+		SetGarbageTrucks(true) -- Stop garbage trucks from randomly spawning
 		SetRandomBoats(false) -- Stop random boats from spawning in the water.
 		SetCreateRandomCops(false) -- disable random cops walking/driving around.
 		SetCreateRandomCopsNotOnScenarios(false) -- stop random cops (not in a scenario) from spawning.
